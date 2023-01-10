21a0ceb20e104238260211610bfef1c999554cb7
Farzad Torkaman <farzad.tkm@gmail.com>
2023-01-10 10:24:28 -0800

    Add project files.

---
+++ Accounting.DataLayer/Repository/ICustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repository {
+    public interface ICustomerRepository {
+
+        List<Customers> GetAllCustomers();
+        Customers GetCustomerById(int customerId);
+        
+        bool InsertCustomer(Customers customer);
+
+        bool UpdateCustomer(Customers customer);
+
+        bool DeleteCustomer(int customerId);
+
+        bool DeleteCustomer(Customers customer);
+
+        /// <summary>
+        /// When ever I do any action in my program I call this methode
+        /// to save it with using lambada expression
+        /// </summary>
+        void save();
+    }
+}
